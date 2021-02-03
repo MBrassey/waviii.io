760726166c1790c6602e0838b114ae4d37f63d63
MBrassey <luc1d_life@protonmail.com>
2021-02-02 17:11:10 -0700

    updated

---
+++ build/precache-manifest.886e0584b26cdf4c53ce621c7cc758b9.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ac953a4d5c6462d9b65b6af21fb7c65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45dc1166d4f010cf0c6f",
+    "url": "/static/css/main.59ad27ff.chunk.css"
+  },
+  {
+    "revision": "42be136cc46780ffb46b",
+    "url": "/static/js/2.b816f13b.chunk.js"
+  },
+  {
+    "revision": "2d7f25a4b61fc80e2f4f575f827efb96",
+    "url": "/static/js/2.b816f13b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45dc1166d4f010cf0c6f",
+    "url": "/static/js/main.ff891571.chunk.js"
+  },
+  {
+    "revision": "a1f19b944ede8fba8cf3",
+    "url": "/static/js/runtime-main.bd6a72e7.js"
+  },
+  {
+    "revision": "eaf789f548e48623aa6f7dff738d5a56",
+    "url": "/static/media/MBrassey-Logo.eaf789f5.png"
+  },
+  {
+    "revision": "7497d710f32e5a572bdaddcb881facd6",
+    "url": "/static/media/eth-logo.7497d710.png"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "5e68c07dae3971fc939ee3e67c601b9b",
+    "url": "/static/media/sContract.5e68c07d.png"
+  },
+  {
+    "revision": "e9dd609157e1e6ae51d61eda905eb48b",
+    "url": "/static/media/swap_logo.e9dd6091.gif"
+  }
+]);
