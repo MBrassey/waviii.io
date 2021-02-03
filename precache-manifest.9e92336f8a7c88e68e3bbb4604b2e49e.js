b08b69d2e6722846fdde97866a15434d58c60f0b
MBrassey <luc1d_life@protonmail.com>
2021-02-02 17:50:02 -0700

    Updates

---
+++ precache-manifest.9e92336f8a7c88e68e3bbb4604b2e49e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e59b4d6ea7bcebfdf9c3a3c23d0c17c",
+    "url": "/waviii.io/index.html"
+  },
+  {
+    "revision": "54d86394c589f0181780",
+    "url": "/waviii.io/static/css/main.0ce89e64.chunk.css"
+  },
+  {
+    "revision": "e4c692f76b32eb6bcad1",
+    "url": "/waviii.io/static/js/2.5c18c1d1.chunk.js"
+  },
+  {
+    "revision": "2d7f25a4b61fc80e2f4f575f827efb96",
+    "url": "/waviii.io/static/js/2.5c18c1d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54d86394c589f0181780",
+    "url": "/waviii.io/static/js/main.ff891571.chunk.js"
+  },
+  {
+    "revision": "9912eaf03b722a493009",
+    "url": "/waviii.io/static/js/runtime-main.49dd3750.js"
+  },
+  {
+    "revision": "eaf789f548e48623aa6f7dff738d5a56",
+    "url": "/waviii.io/static/media/MBrassey-Logo.eaf789f5.png"
+  },
+  {
+    "revision": "7497d710f32e5a572bdaddcb881facd6",
+    "url": "/waviii.io/static/media/eth-logo.7497d710.png"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/waviii.io/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/waviii.io/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/waviii.io/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/waviii.io/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "5e68c07dae3971fc939ee3e67c601b9b",
+    "url": "/waviii.io/static/media/sContract.5e68c07d.png"
+  },
+  {
+    "revision": "e9dd609157e1e6ae51d61eda905eb48b",
+    "url": "/waviii.io/static/media/swap_logo.e9dd6091.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.886e0584b26cdf4c53ce621c7cc758b9.js"
+  "/waviii.io/precache-manifest.9e92336f8a7c88e68e3bbb4604b2e49e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/waviii.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
